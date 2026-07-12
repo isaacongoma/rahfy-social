3d96640b8113818c749a05431d971353d79aa75d
Isaac Ongoma <isaacshisia@gmail.com>
2026-07-12 16:27:14 +0300

    first commit

---
+++ lib/utilities/const_res.dart
@@ -0,0 +1,9 @@
+const String baseURL = 'https://aps.rahfy.com/';
+const String apiURL = '${baseURL}api/';
+const String apiKey = 'retry123';
+
+// If you change this topic you also change backend .env file
+String notificationTopic = "shortzz";
+
+String revenueCatAndroidApiKey = "goog_skiwDfcSTkHavklezeMXnlEbTni"; // revenueCat android api
+String revenueCatAppleApiKey = "________"; // revenueCat apple api
